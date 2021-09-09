d2a6aa59f5ea8cf876ec5cc2bc37cb30bc4d7369
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-09 21:34:42 +0200

    Se Task 3 på trello. Dette er et class som skal ha i seg billett attributter (pris, dato, navn, adresse, billett type..)

---
+++ WebApp_Del1/Models/Billett.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+/// <summary>
+/// Dette er et class som skal ha i seg billett attributter (pris, dato, navn, adresse, billett type..)
+/// </summary>
+public class Billett
+{
+	
+}
