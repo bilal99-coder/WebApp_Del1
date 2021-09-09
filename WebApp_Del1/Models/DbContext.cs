b38bffeb6d85d0a66ff52b3da41566dd3cab9e2f
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-09 21:37:18 +0200

    This a class that will help us organise our tabels /// This will be the implementation of task 3

---
+++ WebApp_Del1/Models/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for DbContext
+/// This a class that will help us organise our tabels
+/// This will be the implementation of task 3 
+/// </summary>
+public class DbContext
+{
+	
+}
