b947f2a11264afe5092eaaf21cf306bad3904744
Robert Larsen <robertlarsenn@gmail.com>
2021-09-11 19:43:34 +0200

    kun 1 kontroller

--- WebApp_Del1/Controllers/BillettController.cs
+++ WebApp_Del1/Controllers/BillettController.cs
@@ -1,10 +1,35 @@
+using billetter.Model;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebApp_Del1.Controllers
 {
 	[Route("[Controller]/[action]")]
 	public class BillettController : ControllerBase
     {
+        private List<Lugar> lugarer;
+        private Billett billett;
 
+        public BillettController()
+        {
+            this.lugarer = new List<Lugar>();
+            Lugar lugar = new Lugar();
+
+        }
+
+
+
+        [Route("{id}")]
+        public void velgLugar(int id)
+        {
+            this.billett.lugarId = id;
+
+
+        }
+
+        public List<Lugar> hentLugarer()
+        {
+            return this.lugarer;
+        }
     }
 }
--- WebApp_Del1/Startup.cs
+++ WebApp_Del1/Startup.cs
@@ -1,10 +1,6 @@
-
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,25 +8,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace WebApp_Del1
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddSessionStateTempDataProvider();
-
-            services.AddSession();
-
-            services.AddRazorPages();
+            services.AddControllers();
+         //   services.AddDbContext<KundeContext>(options => options.UseSqlite("Data source = Kunde.db")); //Skal endres etterhvert.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,26 +28,14 @@ namespace WebApp_Del1
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseSession();
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(); // merk denne!
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapRazorPages();
             });
         }
     }
