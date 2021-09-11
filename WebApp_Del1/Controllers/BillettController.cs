2e93230286f94f10443b93e0f1f36167ba92b203
Robert Larsen <robertlarsenn@gmail.com>
2021-09-11 20:24:23 +0200

    gg

--- WebApp_Del1/Controllers/BillettController.cs
+++ WebApp_Del1/Controllers/BillettController.cs
@@ -1,10 +1,43 @@
+using billetter.Model;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebApp_Del1.Contexts;
 
 namespace WebApp_Del1.Controllers
 {
 	[Route("[Controller]/[action]")]
 	public class BillettController : ControllerBase
     {
+        private Billett billett;
 
+        private readonly BillettContext _lugDb;
+
+        public BillettController()
+        {
+            Lugar lugar = new Lugar();
+
+        }
+
+
+
+        [Route("{id}")]
+        public void velgLugar(int id)
+        {
+            this.billett.lugarId = id;
+
+
+        }
+
+        public List<Lugar> hentLugarer()
+        {
+            return _lugDb.lugarer.ToList();
+        }
+
+        public void registrerBillett()
+        {
+            _lugDb.billetter.Add(billett);
+            _lugDb.SaveChanges();
+        }
     }
 }
