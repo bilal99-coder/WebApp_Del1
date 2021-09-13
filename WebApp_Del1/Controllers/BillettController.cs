167e011370601490c97b4d46a6c97967506f6ba6
Robert Larsen <robertlarsenn@gmail.com>
2021-09-13 12:56:17 +0200

    ...

--- WebApp_Del1/Controllers/BillettController.cs
+++ WebApp_Del1/Controllers/BillettController.cs
@@ -41,16 +41,17 @@ namespace WebApp_Del1.Controllers
         public void velgLugar(int id)
         {
             Lugar lugar = _lugDb.lugarer.Find(id);
-            
+            if (lugar != null)
+            {
                 BillettLugar billettLugar = new BillettLugar();
-                billettLugar.billettId = this.billett.billettId;
+                billettLugar.billettId    = this.billett.billettId;
                 billettLugar.lugar = lugar;
                 billettLugar.lugarId = lugar.lugarId;
                 lugarer.Add(billettLugar);
 
+            }
 
 
-            
 
         }
 
