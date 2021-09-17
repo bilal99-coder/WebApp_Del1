e31d232fcc51a0d1bd07512dfa110356bbb994ba
Robert Larsen <robertlarsenn@gmail.com>
2021-09-17 14:55:58 +0200

    javascript for filter

---
+++ WebApp_Del1/wwwroot/js/filterData.js
@@ -0,0 +1,56 @@
+﻿$(() => {
+    $("#sok").click((e) => {
+        e.preventDefault();
+        let filterData = GUIModule.extractFiltrering();
+        CommunicationModule.sendFilterData(filterData);
+    });
+
+
+});
+
+
+
+var CommunicationModule = (function () {
+
+    async function sendFilterData(filterData) {
+
+
+        await $.post("/billett/filterData",filterData, (res) => {
+            
+        }).promise();
+
+
+    }
+
+    return {
+
+        sendFilterData: function (filterData) {
+
+            sendFilterData(filterData).then((x) => { },
+                (error) => {
+                    setTimeout(() => { CommunicationModule.sendFilterData(filterData) }, 10000);
+                });
+        }
+    }
+})();
+    
+
+var GUIModule = (function () {
+
+    return {
+        extractFiltrering: function () {
+       
+            let filterData = {
+                prisMin:  $("#prisMin").val(),
+                prisMaks: $("#prisMaks").val(),
+                antall:   $("#antall").val(),
+                harWc: $("#wc").prop("checked"),
+                harDysj: $("#dysj").prop("checked"),
+                harWifi: $("#wifi").prop("checked")
+
+            }
+            return filterData;
+
+        }
+    };
+})();
