80671087422b3db1ccfe625774af704f363419ba
HalvorHetland <70580505+HalvorHetland@users.noreply.github.com>
2021-09-20 14:02:47 +0200

    * index.html:
    * index.js: begynnt på index.js

--- WebApp_Del1/wwwroot/js/index.js
+++ WebApp_Del1/wwwroot/js/index.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(() => {
+    $("#reg").click((e) => {
+        alert("hei");
+    });
+});
+/*function ValiderReise() {
+    const fra = $('#fra option:selected').val();
+    const til = $('#til option:selected').val();
+
+    if (fra == til) {
+        $("#fraFeil").html("Feil! fra og til kan ikke være den samme")
+    } else {
+        sokOgBestill();
+    }
+}
+
+function sokOgBestill() {
+    const info = {
+        reisetype: $('#reisetype option:selected').val(),
+        fra: $('#fra option:selected').val(),
+        til: $('#til option:selected').val(),
+        utreise: $("#utreise").val(),
+        antall: $("#antall").val()
+    };
+}
