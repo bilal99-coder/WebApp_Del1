e7ee93986416ba3edb37dd31ad888130b6b6c5a2
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-28 18:24:51 +0200

    Blir her noen endringer:  1) Laget en  ny mappe som heter DAL(Data access layers) som skal erstatte Contexts mappe. 2) Laget en ny klasse inn i DAL Mappen som heter DBInit: _  Det jeg har forstått er at den er den klassen som har databser som har info som vi trenger når applikasjonen instansieres. Det som er bra med den klasseen er at du kan her ifra populere datasbasene som blir kalt når applikasjonen kjøres.

---
+++ WebApp_Del1/DAL/DBInit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_Del1.DAL
+{
+    public class DBInit
+    {
+        public static void Initialize(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<BillettContext>();
+
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated(); 
+            }
+        }
+    }
+}
