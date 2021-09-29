b547ebb90283f80bba2b43995f8218e1bf452970
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-29 20:24:35 +0200

    Revert "Fixed the damaged file now endeeelig"
    This reverts commit 5b9d38e7983f1370873e8d70759fb36feeb77c76.

--- WebApp_Del1/DAL/BillettContext.cs
+++ WebApp_Del1/DAL/BillettContext.cs
@@ -98,20 +98,13 @@ namespace WebApp_Del1.DAL
     {
         [Key]
         public int RuteId { get; set; }
-        public Havn avgangHavnen { get; set; }
-        public virtual List<Havn> ankomstHavner { get; set; }
+        //fra er avgang sted 
+        public string fra { get; set; }
+        //til er distinasjon
+        public string til { get; set; }
         public int pris { get; set; }
 
     }
-
-
-    public class Havn
-    {
-        public int HavnId { get; set; }
-        public string HavnNavn { get; set; }
-
-    }
-    
 
 
 
@@ -128,7 +121,6 @@ namespace WebApp_Del1.DAL
         public DbSet<Personer> personer { get; set; }
         public DbSet<Betalere> Betalere { get; set; }
         public DbSet<Ruter> Ruter { get; set; }
-        public DbSet<Havn> AlleHavner { get; set; }
     
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
